e4e5dc5620516e32aaa3de798bda68d0746f4ebb
david@coppicecapital.co.uk <david@coppicecapital.co.uk>
2023-05-08 19:16:32 +0100

    Initial commit

---
+++ MonthlyStockAnalysis.py
@@ -0,0 +1,38 @@
+import openai
+import pandas as pd
+from yahoo_fin import stock_info
+
+# Replace your_api_key with your actual OpenAI API key
+openai.api_key = "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"
+
+ticker = 'AAPL'
+analyst_info = stock_info.get_analysts_info(ticker)
+
+# Save Growth Estimates to a CSV file
+growth_estimates = analyst_info["Growth Estimates"]
+growth_estimates.to_csv("growth_estimates.csv")
+
+# Read the CSV file
+growth_estimates_df = pd.read_csv("growth_estimates.csv")
+
+# Get the current quarter growth estimate for the stock
+current_qtr_growth_estimate = growth_estimates_df.loc[growth_estimates_df['Growth Estimates'] == 'Current Qtr.', ticker].values[0]
+
+# Send the growth estimate to the OpenAI API. Would prefer to send all in one go but it exceeds the token limits so splitting up to stay below them
+def ask_openai(question):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=(f"{question} The growth estimate for {ticker} in the current quarter is {current_qtr_growth_estimate}. "
+                "Is this a bullish or bearish signal? Please provide a short response."),
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    answer = response.choices[0].text.strip()
+    return answer
+
+question = "Analyze the stock data and tell me"
+response = ask_openai(question)
+print(response)
